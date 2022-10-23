61733be488d9de5caf6381a07616151c61990ffe
Trmor <nhjabvqaz@gmail.com>
2022-10-23 16:45:30 +0400

    final

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const express = require("express");
 const expressHbs = require("express-handlebars");
 const hbs = require("hbs");
+const { TIME } = require("sequelize");
 const app = express();
 const urlencodedParser = express.urlencoded({extended: false});
  
@@ -37,7 +38,6 @@ app.get("/", function(req, res){
         raw: true,
       },
       ).then(data=>{
-      console.log(data);
       res.render("view/index.hbs", {
         users: data
       });
@@ -46,7 +46,6 @@ app.get("/", function(req, res){
 
 app.get("/create/user", function(req, res){
   DB.AccessLevel.findAll({raw:true}).then(data=>{
-    console.log(data);
     res.render("create/user.hbs", {accesslevels: data})})
   .catch(err=>console.log(err));
 });
@@ -75,7 +74,6 @@ app.get("/edit/user/:id", function(req, res){
   });
   const accesslevel = DB.AccessLevel.findAll({raw: true});
   Promise.all([user, accesslevel]).then(data=>{
-    console.log(data);
     res.render("edit/user.hbs", {
       user: data
     });
@@ -134,7 +132,6 @@ app.get("/create/carriage", function(req, res){
   carriagetype = DB.CarriageType.findAll({raw:true});
   cargo = DB.Cargo.findAll({raw:true});
   Promise.all([train, carriagestatus, carriagetype, cargo]).then(data=>{
-    console.log(data);
     res.render("create/carriage.hbs", {carriage:data})
   }).catch(err=>console.log(err));
 });
@@ -176,7 +173,6 @@ app.get("/edit/carriage/:id", function(req, res){
   raw: true
   });
   Promise.all([carriage, train, carriagestatus, carriagetype, cargo]).then(data=>{
-    console.log(data);
     res.render("edit/carriage.hbs", {
       carriage: data
     });
@@ -214,7 +210,215 @@ app.post("/delete/carriage/:id", function(req, res){
   }).catch(err=>console.log(err));
 });
 
-}//carriages module
+}
+
+//cargos module
+{
+  app.get("/cargos", function(req, res){
+    DB.Cargo.findAll(
+      {
+        include: { 
+          model: DB.CargoType,
+        },
+        raw: true,
+      },
+      ).then(data=>{
+      console.log(data);
+      res.render("view/cargos.hbs", {
+        cargos: data
+      });
+    }).catch(err=>console.log(err));
+});
+
+app.get("/create/cargo", function(req, res){
+  DB.CargoType.findAll({raw:true}).then(data=>{
+    console.log(data);
+    res.render("create/cargo.hbs", {cargotypes: data})})
+  .catch(err=>console.log(err));
+});
+ 
+app.post("/create/cargo", urlencodedParser, function (req, res) {
+         
+    if(!req.body) return res.sendStatus(400);
+    
+    const id = req.body.id;
+    const name = req.body.name;
+    const cargotype = req.body.cargotype;
+    if(!req.body.id){
+    DB.Cargo.create({ name: name, cargotypeId: cargotype}).then(()=>{
+      res.redirect("/cargos");
+    }).catch(err=>console.log(err));
+    }
+    else 
+    {
+    DB.Cargo.create({ id: id, name: name, cargotypeId: cargotype}).then(()=>{
+      res.redirect("/cargos");
+    }).catch(err=>console.log(err));
+    }
+});
+
+app.get("/edit/cargo/:id", function(req, res){
+  const id = req.params.id;
+  const cargo = DB.Cargo.findAll({where:{id: id},
+    include: { 
+    model: DB.CargoType,
+  }, 
+  raw: true,
+  });
+  const cargotypes = DB.CargoType.findAll({raw: true});
+  Promise.all([cargo, cargotypes]).then(data=>{
+    res.render("edit/cargo.hbs", {
+      cargo: data
+    });
+  })
+  .catch(err=>console.log(err));
+});
+ 
+app.post("/edit/cargo", urlencodedParser, function (req, res) {
+         
+  if(!req.body) return res.sendStatus(400);
+
+  const id = req.body.id;
+  const name = req.body.name;
+  const cargotype = req.body.cargotype;
+  DB.Cargo.update({name: name, cargotypeId: cargotype}, {where: {id: id} }).then(() => {
+    res.redirect("/cargos");
+  })
+  .catch(err=>console.log(err));
+});
+
+app.post("/delete/cargo/:id", function(req, res){  
+  const cargo = req.params.id;
+  DB.Cargo.destroy({where: {id: cargo} }).then(() => {
+    res.redirect("/cargos");
+  }).catch(err=>console.log(err));
+});
+
+}
+
+//shipment module
+{
+  app.get("/shipments", function(req, res){
+    DB.Shipment.findAll(
+      {
+        include: [
+          {model: DB.User},
+          {model: DB.Train},
+          {model: DB.StartingStation},
+          {model: DB.DestinationStation},
+          {model: DB.ShipmentStatus}
+        ],
+        raw: true,
+      },
+      ).then(data=>{
+      res.render("view/shipments.hbs", {
+        shipments: data
+      });
+    }).catch(err=>console.log(err));
+});
+
+app.get("/create/shipment", function(req, res){
+let user; let shipmentstatus; let startingstation; let destinationstation;
+user = DB.User.findAll({raw: true});
+train = DB.Train.findAll({raw:true});  
+shipmentstatus = DB.ShipmentStatus.findAll({raw: true});
+startingstation = DB.StartingStation.findAll({raw: true});
+destinationstation = DB.DestinationStation.findAll({raw: true});
+Promise.all([user, startingstation, destinationstation, train, shipmentstatus]).then(data=>{
+    res.render("create/shipment.hbs", {shipment: data})})
+  .catch(err=>console.log(err));
+});
+ 
+app.post("/create/shipment", urlencodedParser, function (req, res) {
+         
+    if(!req.body) return res.sendStatus(400);
+    
+    const id = req.body.id;
+    const user = req.body.user;
+    const startingstation = req.body.startingstation;
+    const destinationstation = req.body.destinationstation;
+    const train = req.body.train;
+    const shipmentstatus = req.body.shipmentstatus;
+    if(!req.body.id){
+      DB.Shipment.create({
+        userId: user,
+        startingstationId: startingstation,
+        destinationstationId: destinationstation,
+        trainId: train,
+        shipmentstatusId: shipmentstatus
+      }).then(()=>{
+        res.redirect("/shipments");
+      }).catch(err=>console.log(err));
+    }else{
+      DB.Shipment.create({
+        id: id,
+        userId: user,
+        startingstationId: startingstation,
+        destinationstationId: destinationstation,
+        trainId: train,
+        shipmentstatusId: shipmentstatus
+      }).then(()=>{
+        res.redirect("/shipments");
+      }).catch(err=>console.log(err));
+    }
+});
+ 
+app.get("/edit/shipment/:id", function(req, res){
+  const id = req.params.id;
+  let shipment = DB.Shipment.findAll({where:{id: id},
+    include: [
+      {model: DB.User},
+      {model: DB.Train},
+      {model: DB.StartingStation},
+      {model: DB.DestinationStation},
+      {model: DB.ShipmentStatus}
+    ],
+    raw: true
+  });
+  let user = DB.User.findAll({raw: true});
+  let train = DB.Train.findAll({raw: true});
+  let startingstation = DB.StartingStation.findAll({raw: true});
+  let destinationstation = DB.DestinationStation.findAll({raw:true});
+  let shipmentstatus = DB.ShipmentStatus.findAll({raw: true});
+  Promise.all([shipment ,user, startingstation, destinationstation, train, shipmentstatus]).then(data=>{
+    console.log(data);
+    res.render("edit/shipment.hbs", {
+      shipment: data
+    });
+  })
+  .catch(err=>console.log(err));
+});
+ 
+app.post("/edit/shipment", urlencodedParser, function (req, res) {
+         
+  if(!req.body) return res.sendStatus(400);
+ 
+  const id = req.body.id;
+  const user = req.body.user;
+  const startingstation = req.body.startingstation;
+  const destinationstation = req.body.destinationstation;
+  const train = req.body.train;
+  const shipmentstatus = req.body.shipmentstatus;
+  DB.Shipment.update({
+    userId: user,
+    startingstationId: startingstation,
+    destinationstationId: destinationstation,
+    trainId: train,
+    shipmentstatusId:shipmentstatus
+  }, {where: {id: id} }).then(() => {
+    res.redirect("/shipments");
+  })
+  .catch(err=>console.log(err));
+});
+ 
+app.post("/delete/shipment/:id", function(req, res){  
+  const id = req.params.id;
+  DB.Shipment.destroy({where: {id: id} }).then(() => {
+    res.redirect("/shipments");
+  }).catch(err=>console.log(err));
+});
+
+}
 
 module.exports.close = function(){
   sequelize.close();
